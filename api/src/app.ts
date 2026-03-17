25fb4c7a1d408e698a8fbf2005cbddf35937bff6
João Paulo <jopaulo.as8@gmail.com>
2026-03-17 17:37:07 -0300

    chore(#03-1): configurando o prisma para o projeto

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,33 @@
 import express from 'express'
+import { prisma } from './lib/prisma'
 
 export const app = express()
 
 app.use(express.json())
 
-app.get('/', (_req, res) => {
-  res.json({ status: 'API está funcionando!' })
+app.get('/health', (_req, res) => {
+  res.json({ status: 'ok' })
+})
+
+
+// ---- ROTAS DE EXEMPLO ----
+// Utilizei para testar a instância do Prisma
+
+
+// POST /users
+app.post('/users', async (req, res) => {
+  const { email, name } = req.body
+
+  const user = await prisma.user.create({
+    data: { email, name }
+  })
+
+  res.status(201).json(user)
+})
+
+// GET /users
+app.get('/users', async (_req, res) => {
+  const users = await prisma.user.findMany()
+
+  res.json(users)
 })
