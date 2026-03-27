c93bb2521f4b53800616def81060a6dc10bd80d9
CauãCursino <cmfcursino@gmail.com>
2026-03-27 14:23:00 -0300

    refactor(#03-3): Atualização das rotas para novos campos do banco de dados

--- api/src/controllers/dataController.ts
+++ api/src/controllers/dataController.ts
@@ -2,7 +2,7 @@ import e, {Request, Response} from 'express'
 import {prisma} from '../lib/prisma'
 
 export async function criarDados(request: Request, response: Response) {
-    const {data_base, uf, cliente, cnae_ocupacao, porte, carteira_inadiplencia} = request.body
+    const {data_base, uf, cliente, cnae_ocupacao, porte, carteira_inadiplencia, carteira_vencida} = request.body
 
     if(!data_base || !uf || !cliente){
         return response.status(400).json({error: 'Campos obrigatórios faltando'})
@@ -16,7 +16,8 @@ export async function criarDados(request: Request, response: Response) {
                 cliente, 
                 cnae_ocupacao, 
                 porte, 
-                carteira_inadiplencia
+                carteira_inadiplencia,
+                carteira_vencida
             }
         })
 
@@ -63,6 +64,108 @@ export async function criarDadosPIX(request: Request, response: Response) {
     }
 }
 
+export async function criarDadosTaxaEscolarizacao(request: Request, response: Response) {
+    const { nn, v, d1n, d3n, d5n } = request.body
+
+    if(!nn || !v || !d5n){
+        return response.status(400).json({error: 'Campos obrigatórios faltando'})
+    }
+
+    try{
+        const dado = await prisma.taxaEscolarizacao.create({
+            data: {
+                nn, 
+                v, 
+                d1n, 
+                d3n, 
+                d5n
+            }
+        })
+
+        return response.status(201).json({message: 'Taxa de escolarização recebida e cadastrada com sucesso', dado})
+    }
+
+    catch(error){
+        console.error(error)
+        return response.status(500).json({error: 'Erro ao receber taxa de escolarização'})
+    }
+}
+
+export async function criarDadosCresPopulacional(request: Request, response: Response) {
+    const { d1n, d2n, v } = request.body
+
+    if(!d1n || !v || !d2n){
+        return response.status(400).json({error: 'Campos obrigatórios faltando'})
+    }
+
+    try{
+        const dado = await prisma.crescimentoPopulacional.create({
+            data: {
+                d1n,
+                d2n,
+                v
+            }
+        })
+
+        return response.status(201).json({message: 'Crescimento populacional recebido e cadastrado com sucesso', dado})
+    }
+
+    catch(error){
+        console.error(error)
+        return response.status(500).json({error: 'Erro ao receber crescimento populacional'})
+    }
+}
+
+export async function criarDadosPopAbsoluta(request: Request, response: Response) {
+    const { d1n, d2n, v } = request.body
+
+    if(!d1n || !v || !d2n){
+        return response.status(400).json({error: 'Campos obrigatórios faltando'})
+    }
+
+    try{
+        const dado = await prisma.populacaoAbsoluta.create({
+            data: {
+                d1n,
+                d2n,
+                v
+            }
+        })
+
+        return response.status(201).json({message: 'População Absoluta recebida e cadastrada com sucesso', dado})
+    }
+
+    catch(error){
+        console.error(error)
+        return response.status(500).json({error: 'Erro ao receber população absoluta'})
+    }
+}
+
+export async function criarDadosBonusDemografico(request: Request, response: Response) {
+    const { brasilGrandeRegiaoUf, total, xy_anos } = request.body
+
+    if(!brasilGrandeRegiaoUf || !total || !xy_anos){
+        return response.status(400).json({error: 'Campos obrigatórios faltando'})
+    }
+
+    try{
+        const dado = await prisma.bonusDemografico.create({
+            data: {
+                brasilGrandeRegiaoUf, 
+                total, 
+                xy_anos
+            }
+        })
+
+        return response.status(201).json({message: 'Bônus Demográfico recebido e cadastrado com sucesso', dado})
+    }
+
+    catch(error){
+        console.error(error)
+        return response.status(500).json({error: 'Erro ao receber Bônus Demográfico'})
+    }
+}
+
 export async function listarDados(request: Request, response: Response) {
   try {
     const dados = await prisma.dados.findMany()
@@ -78,5 +181,41 @@ export async function listarDadosPIX(request: Request, response: Response) {
     return response.json(dadosPIX)
   } catch (error) {
     return response.status(500).json({ error: 'Erro ao buscar dados PIX' })
+  }
+}
+
+export async function listarTaxaEscolarizacao(request: Request, response: Response) {
+  try {
+    const taxaEsc = await prisma.taxaEscolarizacao.findMany()
+    return response.json(taxaEsc)
+  } catch (error) {
+    return response.status(500).json({ error: 'Erro ao buscar Taxa de Escolarização' })
+  }
+}
+
+export async function listarCrescimentoPopulacional(request: Request, response: Response) {
+  try {
+    const cresPop = await prisma.crescimentoPopulacional.findMany()
+    return response.json(cresPop)
+  } catch (error) {
+    return response.status(500).json({ error: 'Erro ao buscar Crescimento Populacional' })
+  }
+}
+
+export async function listarPopAbsoluta(request: Request, response: Response) {
+  try {
+    const popAbs = await prisma.populacaoAbsoluta.findMany()
+    return response.json(popAbs)
+  } catch (error) {
+    return response.status(500).json({ error: 'Erro ao buscar População Absoluta' })
+  }
+}
+
+export async function listarBonusDemografico(request: Request, response: Response) {
+  try {
+    const bonus = await prisma.bonusDemografico.findMany()
+    return response.json(bonus)
+  } catch (error) {
+    return response.status(500).json({ error: 'Erro ao buscar Bônus Demográfico' })
   }
 }
--- api/src/routes/data.ts
+++ api/src/routes/data.ts
@@ -1,14 +1,23 @@
 import {Router} from 'express'
-import {criarDados, criarDadosPIX, listarDados, listarDadosPIX} from '../controllers/dataController'
+import {criarDados, criarDadosPIX, criarDadosTaxaEscolarizacao, criarDadosCresPopulacional, criarDadosPopAbsoluta, criarDadosBonusDemografico} from '../controllers/dataController'
+import {listarDados, listarDadosPIX, listarTaxaEscolarizacao, listarCrescimentoPopulacional, listarPopAbsoluta, listarBonusDemografico} from '../controllers/dataController'
 import checarCampos from '../middlewares/dataMiddleware'
 
 const routerDados = Router()
 
 routerDados.post('/receber-dados', checarCampos(['data_base', 'uf', 'cliente']), criarDados)
 routerDados.post('/receber-dados-pix', checarCampos(['ano_mes', 'sigla_regiao', 'municipio_ibge']), criarDadosPIX)
+routerDados.post('/receber-taxa', checarCampos(['nn', 'v', 'd5n']), criarDadosTaxaEscolarizacao)
+routerDados.post('/receber-crescimento', checarCampos(['d1n', 'v', 'd2n']), criarDadosCresPopulacional)
+routerDados.post('/receber-populacao', checarCampos(['d1n', 'v', 'd2n']), criarDadosPopAbsoluta)
+routerDados.post('/receber-bonus', checarCampos(['brasilGrandeRegiaoUf', 'total', 'xy_anos']), criarDadosBonusDemografico)
 
 routerDados.get('/listar-dados', listarDados)
 routerDados.get('/listar-dados-pix', listarDadosPIX)
+routerDados.get('/listar-taxa', listarTaxaEscolarizacao)
+routerDados.get('/listar-crescimento', listarCrescimentoPopulacional)
+routerDados.get('/listar-populacao', listarPopAbsoluta)
+routerDados.get('/listar-bonus', listarBonusDemografico)
 
 
 export default routerDados
