30306213763c08256a2f81cc84ad27b90ad661fb
João Paulo <jopaulo.as8@gmail.com>
2026-03-27 22:46:58 -0300

    feat(#04-2): layout genérico da página do dashboard gerado por IA

--- web/app/README.md
+++ web/app/README.md
@@ -2,3 +2,5 @@
 Onde todo o código fonte do projeto frontend web se encontra.
 
 > Dentro do **Next.js**, todos os diretórios que possuem um arquivo ```page.tsx``` são considerados rotas de url.
+
+> Os diretórios com parênteses (()) são "ignorados" na url.
--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="pt-br"
       className={`${catamaran.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
