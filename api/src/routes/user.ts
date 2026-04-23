1246d3b4c7dcbfe4191212ccd440bd96d7c93f33
João Paulo <jopaulo.as8@gmail.com>
2026-04-23 10:02:40 -0300

    feat(#9-3): adicionando a validação de parâmetros no cadastro de usuários

--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -2,15 +2,17 @@ import { Router } from 'express'
 import { register, listAll, getById, getProfile, update, softDelete } from '../controllers/userController'
 import { sessionMiddleware } from '../middlewares/sessionMiddleware'
 import { adminOnly } from '../middlewares/roleMiddleware'
+import { validateData } from '../middlewares/dataMiddleware' // Importe o middleware
+import { createUserSchema, updateUserSchema } from '../schemas/userSchemas' // Importe os schemas
 
 const router = Router()
 
 router.get('/me', sessionMiddleware, getProfile)
 
 router.get('/', sessionMiddleware, adminOnly, listAll)
-router.post('/', sessionMiddleware, adminOnly, register)
+router.post('/', sessionMiddleware, adminOnly, validateData(createUserSchema), register)
 router.get('/:id', sessionMiddleware, adminOnly, getById);
-router.put('/:id', sessionMiddleware, adminOnly, update)
+router.put('/:id', sessionMiddleware, adminOnly, validateData(updateUserSchema), update)
 router.patch('/:id/inactivate', sessionMiddleware, adminOnly, softDelete)
 
 export default router
--- api/src/schemas/userSchemas.ts
+++ api/src/schemas/userSchemas.ts
@@ -1,9 +1,17 @@
 import { z } from 'zod';
 
 export const createUserSchema = z.object({
-  name: z.string().optional(),
-  email: z.email(),
-  password: z.string().min(8),
+  name: z.string()
+    .min(1, "O nome não pode ser nulo ou vazio")
+    .regex(/^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/, "O nome não deve conter caracteres especiais ou números"),
+  
+  email: z.email("E-mail inválido (deve conter @ e conteúdo antes e depois)")
+    .min(1, "O e-mail é obrigatório"),
+  
+  password: z.string()
+    .min(8, "A senha deve ter no mínimo 8 caracteres")
+    .regex(/^[a-zA-Z0-9]+$/, "A senha não deve conter caracteres especiais"),
+    
   status: z.enum(['ADMIN', 'ACTIVE', 'INACTIVE']).optional()
 });
 
