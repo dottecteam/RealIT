d9211509ca1e212d0cb46362b9fe2f35b414792a
João Paulo <jopaulo.as8@gmail.com>
2026-04-24 11:03:05 -0300

    fix: ajustes nos schemas

--- api/src/schemas/authSchemas.ts
+++ api/src/schemas/authSchemas.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 
 export const loginSchema = z.object({
-  email: z.email("E-mail inválido"),
-  password: z.string().min(8, "A senha deve ter no mínimo 8 caracteres"),
+  body: z.object({
+    email: z.email("E-mail inválido"),
+    password: z.string().min(8, "A senha deve ter no mínimo 8 caracteres")
+  })
 });
